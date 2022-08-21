30a61fff46d6aa91821c9f6c3b8ca8edd09a6f7c
BytesC0de <110985128+BytesC0de@users.noreply.github.com>
2022-08-21 16:41:21 +0200

    Made it work LOL

--- me/bytesC0de/AntiCrackedLauncher/AntiCrackedLauncher.java
+++ me/bytesC0de/AntiCrackedLauncher/AntiCrackedLauncher.java
@@ -7,7 +7,7 @@ public class AntiCrackedLauncher {
 	public static void checkForCrackedLauncher(Session session, boolean bypassCheck, CrackedLauncherDetectedAction action) {
 		if(!bypassCheck) {
 			action.preCheck();
-			if(session.getPlayerID() == "" || session.getPlayerID() == "" || session.getToken() == "0" || session.getSessionID() == null || session.getSessionID() == "" || (session.getSessionType() != Session.Type.LEGACY && session.getSessionType() != Session.Type.MOJANG)) {
+			if(session.getToken() == "0") {
 				action.onDetect();
 			} else {
 				action.onNotDetected();
